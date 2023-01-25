6b2c30508254b85a18c681263c9a7ecf7d208232
chorockuin <chorockuin@gmail.com>
2023-01-25 22:44:22 +0900

    Add 02079
    -------------------------------------------------|
    ------------------------------------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Create : feature
        - Refactor : feature and not testcase
        - Update : feature and testcase
        - Delete : feature
        - Fix : bugs
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

---
+++ 02079.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        distance = 0
+        water_can = capacity
+        x = 0
+        for i, water_plant in enumerate(plants):
+            if water_can < water_plant:
+                water_can = capacity
+                x += distance * 2
+            water_can -= water_plant
+            distance += 1
+            x += 1
+        return x
+
+print(Solution().wateringPlants([2,2,3,3], 5)) # 14
+print(Solution().wateringPlants([1,1,1,4,2,3], 4)) # 30
+print(Solution().wateringPlants([7,7,7,7,7,7,7], 8)) # 49
