98399292a9ce1786e213127662dd94fc25abf24f
chorockuin <chorockuin@gmail.com>
2023-02-26 16:50:13 +0900

    Refactor 로그래머스 게임맵 최단거리
    -------------------------------------------------|
    ------------------------------------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Create : feature
        - Refactor : feature and not testcase
        - Update : feature and testcase
        - Delete : feature
        - Fix : bugs
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

--- prgm00015.py
+++ prgm00015.py
@@ -10,6 +10,7 @@ def bfs(maps):
     m = len(maps[0])
 
     q = [[0, 0, 1]]
+    maps[0][0] = 0
 
     while len(q) > 0:
         inf = q.pop(0)
@@ -21,42 +22,14 @@ def bfs(maps):
         if y == n-1 and x == m-1:
             return c
         
-        maps[y][x] = 0
-        
         for pos in [[y, x-1], [y, x+1], [y-1, x], [y+1, x]]:
             if can_go(pos, n, m) and maps[pos[0]][pos[1]] == 1:
                 q.append([pos[0], pos[1], c+1])
+                maps[pos[0]][pos[1]] = 0
     return -1
-
-def dfs_sub(pos, n, m, maps):
-    y = pos[0]
-    x = pos[1]
-    
-    if can_go(pos, n, m) == False or maps[y][x] == 0:
-        return False
-    
-    if y == n-1 and x == m-1:
-        return True
-    
-    maps[y][x] = 0
-    
-    return dfs_sub([y, x-1], n, m, maps) or dfs_sub([y, x+1], n, m, maps) or dfs_sub([y-1, x], n, m, maps) or dfs_sub([y+1, x], n, m, maps)
-    
-
-def dfs(maps):
-    n = len(maps)
-    m = len(maps[0])
-    
-    return dfs_sub([0, 0], n, m, maps)
-    
-import copy
 
 def solution(maps):
-    m1 = copy.deepcopy(maps)
-    m2 = copy.deepcopy(maps)
-    if dfs(m1):
-        return bfs(m2)
-    return -1
+    return bfs(maps)
 
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
