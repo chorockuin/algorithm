6f97b4ea3970fa30d11e867c1d30e86aa67e730b
chorockuin <chorockuin@gmail.com>
2023-03-12 13:02:58 +0900

    Refactor 프로그래머스 디펜스게임
    우선순위 큐를 사용하여 시간 최적화
    
    -------------------------------------------------|
    ------------------------------------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Create : feature
        - Refactor : feature and not testcase
        - Update : feature and testcase
        - Delete : feature
        - Fix : bugs
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

--- prgm00018.py
+++ prgm00018.py
@@ -1,13 +1,55 @@
-def solution(n, k, enemy):
-    killed_enemy = []
-    for i in range(len(enemy)):
-        killed_enemy.append(enemy[i])
-        killed_enemy.sort(reverse=True)
-        if len(killed_enemy) > k:
-            if sum(killed_enemy[k:]) > n:
+# def reverse_insort(a, x):
+#     i = 0
+#     size = len(a)
+#     while i < size:
+#         mid = (i + size)//2
+#         if x > a[mid]:
+#             size = mid
+#         else:
+#             i = mid + 1
+#     a.insert(i, x)
+
+# def solution(bullet_num, bomb_num, enemy_num_list):
+#     enemy_num = 0
+#     top_enemy_num_list = []
+#     top_enemy_num_list_sum = 0
+#     for i in range(len(enemy_num_list)):
+#         enemy_num += enemy_num_list[i]
+#         if i < bomb_num:
+#             reverse_insort(top_enemy_num_list, enemy_num_list[i])
+#             top_enemy_num_list_sum += enemy_num_list[i]
+#         else:
+#             if enemy_num_list[i] > top_enemy_num_list[-1]:
+#                 reverse_insort(top_enemy_num_list, enemy_num_list[i])
+#                 top_enemy_num_list_sum += enemy_num_list[i] - top_enemy_num_list[-1]
+#                 top_enemy_num_list.pop(-1)
+
+#             if enemy_num - top_enemy_num_list_sum > bullet_num:
+#                 return i
+#     return len(enemy_num_list)
+
+from queue import PriorityQueue
+
+def solution(bullet_num, bomb_num, enemy_num_list):
+    enemy_num = 0
+    top_enemy_num_q = PriorityQueue()
+    top_enemy_num_q_sum = 0
+    for i in range(len(enemy_num_list)):
+        enemy_num += enemy_num_list[i]
+        if i < bomb_num:
+            top_enemy_num_q.put(enemy_num_list[i])
+            top_enemy_num_q_sum += enemy_num_list[i]
+        else:
+            min_enemy_num = top_enemy_num_q.get()
+            if enemy_num_list[i] > min_enemy_num:
+                top_enemy_num_q.put(enemy_num_list[i])
+                top_enemy_num_q_sum += enemy_num_list[i] - min_enemy_num
+            else:
+                top_enemy_num_q.put(min_enemy_num)
+
+            if enemy_num - top_enemy_num_q_sum > bullet_num:
                 return i
-    return len(enemy)
-            
+    return len(enemy_num_list)
 
 print(solution(7, 3, [4,2,4,5,3,3,1])) # 5
 print(solution(2, 4, [3,3,3,3])) # 4
