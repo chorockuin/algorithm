757ff353fccffc6ef16cd6033f52a7e1598d724d
chorockuin <chorockuin@lginnotek.com>
2023-03-21 08:59:48 +0900

    Refactor 프로그래머스 두 큐 합 같게 만들기
    큐 작업 대신 index를 사용하여 최적화
    
    -------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Add : new features
        - Update : feature
        - Remove : feature
        - Refactor : feature's structure not testcase
        - Fix : bug
        - Verify : feature with testcase
    ------------------------------------------------------------------------------|
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

--- prgm00023.py
+++ prgm00023.py
@@ -2,26 +2,32 @@ def solution(q1, q2):
     q1_sum = sum(q1)
     q2_sum = sum(q2)
     
+    max_len = len(q1)+len(q2)
+    
     if (q1_sum + q2_sum)%2 == 1:
         return -1
+    
+    q1_i = 0
+    q2_i = 0
     
     times = 0
     while True:
         if q1_sum == q2_sum:
             break
-        elif q1_sum > q2_sum:
-            q1_sum -= q1[0]
-            q2_sum += q1[0]
-            q2.append(q1[0])
-            q1 = q1[1:]
-            if len(q1) == 0:
+        
+        if q1_sum > q2_sum:
+            q1_sum -= q1[q1_i]
+            q2_sum += q1[q1_i]
+            q2.append(q1[q1_i])
+            q1_i += 1
+            if q1_i >= len(q1) or q1_i >= max_len:
                 return -1
         else:
-            q2_sum -= q2[0]
-            q1_sum += q2[0]
-            q1.append(q2[0])
-            q2 = q2[1:]
-            if len(q2) == 0:
+            q2_sum -= q2[q2_i]
+            q1_sum += q2[q2_i]
+            q1.append(q2[q2_i])
+            q2_i += 1
+            if q2_i >= len(q2) or q1_i >= max_len:
                 return -1
         times += 1
     return times
@@ -29,3 +35,4 @@ def solution(q1, q2):
 print(solution([3, 2, 7, 2], [4, 6, 5, 1])) # 2
 print(solution([1, 2, 1, 2], [1, 10, 1, 2])) # 7
 print(solution([1, 1], [1, 5])) # -1
+print(solution([4, 2], [3, 7])) # -1
